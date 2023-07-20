da06072c422d5bc86a390d66ca88eb80ca52824d
MayankBhadeshiya <mayankbhadeshiya2@gmail.com>
2023-07-20 19:28:52 +0530

    Responsive

--- ios/MovieAndTvTests/MovieAndTvTests.m
+++ ios/MovieAndTvTests/MovieAndTvTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeExamTests : XCTestCase
+@interface MovieAndTvTests : XCTestCase
 
 @end
 
-@implementation ReactNativeExamTests
+@implementation MovieAndTvTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
